434c66ba4edaaaa175d04c45101f4c710360f5f5
Victor L. Bueno <110859187+VictorlBueno@users.noreply.github.com>
2022-10-21 17:53:47 -0300

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,22 +1,29 @@
+from functools import reduce
 from word_decrypt import *
 
-descript = list(input("Decrypt: "))
+keys = ["‡","ɬ","ɨ","ɍ","⑆","⑇","།","༑","༎","༏"]
+keyC = list(input("Choose a key: "))
+keyC = [int(num) for num in keyC]
+key = reduce(lambda x, y:x+y, keyC)
+key = int(repr(key)[-1])
+
+descript = list(input("Texto to decrypt: "))
 encrypted = []
 md(descript,encrypted)
 encrypted.reverse()
 
-
 for i in range(6):
-    encrypted.append("∷"*(1*i)+"*"*(encrypted.index("∷")))
+    encrypted.append(key*(1*i)+"*"*(encrypted.index(key)))
 
 ebackup = list(encrypted)
 password = []
+
 while len(encrypted) > 0:  
-    while encrypted[0] == "∷":
+    while encrypted[0] == key or encrypted[0] == "⧽" or encrypted[0] == "⥾" or encrypted[0] == "⇞" or encrypted[0] == "❚" or encrypted[0] == "˫":
         encrypted.pop(0)
 
-    if "∷" in encrypted:
-        ind = encrypted.index("∷")
+    if key in encrypted:
+        ind = encrypted.index(key)
         password.append(encrypted[:ind])
         for rm in range(ind):
             encrypted.pop(0)
@@ -33,7 +40,7 @@ for l in range(len(password[0])):
     for c in range(len(password)):
         descript.append(password[c][l])
 
-descript = list(filter(lambda i: i not in "*" not in i !="∷", descript))
+descript = list(filter(lambda i: i not in "*" not in i !=key not in i !="⧽" not in i !="⥾" not in i !="⇞" not in i !="❚" not in i !="˫", descript))
 
 descript = "".join(descript)
 print(descript)
